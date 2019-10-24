af22ab667bb4097e11aeb8a41a17fd7093203504
Cahyanaalhafiz <cahyanaalhafiz008@gmail.com>
2019-10-24 16:18:49 +0700

    UTS-DASAR-PEMROGRAMAN

---
+++ BAB II/deklarasi.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+int jumlahuang ;
+
+jumlahuang = 1000;
+cout << "jumlah uang semula: " << jumlahuang <<endl;
+
+// Variabel diubah
+
+jumlahuang = 1200;
+cout << "Jumlah uang sekarang " << jumlahuang << endl;
+
+return 0;
+}
